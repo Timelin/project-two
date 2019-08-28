7cb163f876b4dd845cf169b639daa1e8af25885a
Linjiaqiang <3108136635@qq.com>
2019-08-28 11:25:39 +0800

    day09:一.Spring Data Redis 的示例工程：mage-redis,单机版和集群版; 二.magelala项目整合Redis:1.在magelala-common整合Redis(主要是单机版)；2.网站首页-广告缓存功能：1.1 读取缓存；1.2新增广告后清除缓存；1.3修改灌高后清除缓存；1.4删除广告后清除缓存。

---
+++ mage-redis/src/test/java/com/mage/redis/RedisTest.java
@@ -0,0 +1,69 @@
+package com.mage.redis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @ClassName:RedisTest
+ * @Author:Timelin
+ **/
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:spring/applicationContext-redis.xml")
+public class RedisTest {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    // 测试字符串objing
+    @Test
+    public void testString(){
+        redisTemplate.boundValueOps("string_key").set("马格啦啦");
+        Object obj = redisTemplate.boundValueOps("string_key").get();
+        System.out.println(obj);
+    }
+
+    // 测试散列hash,有序
+    @Test
+    public void testHash(){
+        redisTemplate.boundHashOps("hash_key").put("f1","v1");
+        redisTemplate.boundHashOps("hash_key").put("f2","v2");
+        List list = redisTemplate.boundHashOps("hash_key").values();
+        System.out.println(list);
+    }
+
+    // 测试列表list
+    @Test
+    public void testList(){
+        redisTemplate.boundListOps("list_key").leftPush(1);
+        redisTemplate.boundListOps("list_key").leftPush(2);
+        List list_key = redisTemplate.boundListOps("list_key").range(0, -1);//-1是包含全部的
+
+        System.out.println(list_key);
+    }
+
+    // 测试集合set,无序
+    @Test
+    public void testSet(){
+        redisTemplate.boundSetOps("set_key").add(1,3,5,"mage",7);
+        Set set_key = redisTemplate.boundSetOps("set_key").members();
+        System.out.println(set_key);
+    }
+
+    // 测试有序集合sorted set
+    @Test
+    public void testSortedSet(){
+        redisTemplate.boundZSetOps("zset_key").add("aa",20);
+        redisTemplate.boundZSetOps("zset_key").add("bb",20);
+        redisTemplate.boundZSetOps("zset_key").add("cc",30);
+        Set zset_key = redisTemplate.boundZSetOps("zset_key").range(0, -1);// -1是包括所有的
+        System.out.println(zset_key);
+
+    }
+}
--- magelala/magelala-content/magelala-content-service/src/main/java/com/magelala/content/service/impl/ContentServiceImpl.java
+++ magelala/magelala-content/magelala-content-service/src/main/java/com/magelala/content/service/impl/ContentServiceImpl.java
@@ -9,9 +9,12 @@ import com.magelala.pojo.TbContent;
 import com.magelala.service.impl.BaseServiceImpl;
 import com.magelala.vo.PageResult;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.util.StringUtils;
 import tk.mybatis.mapper.entity.Example;
 
+import java.io.Serializable;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -23,6 +26,11 @@ public class ContentServiceImpl extends BaseServiceImpl<TbContent>implements Con
 
     @Autowired
     private ContentMapper contentMapper;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+    private static final String REDIS_CONTENT="content";
+
 
     //条件搜索
     @Override
@@ -47,7 +55,17 @@ public class ContentServiceImpl extends BaseServiceImpl<TbContent>implements Con
     **/
     @Override
     public List<TbContent> findContentListByCategoryId(Long categoryId) {
+        List<TbContent> list = null;
 
+        // 先从Redis缓存中查找
+        try {
+            list =(List<TbContent>) redisTemplate.boundHashOps(REDIS_CONTENT).get(categoryId);
+            if(list !=null){
+                return list;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         Example example = new Example(TbContent.class);
         Example.Criteria criteria = example.createCriteria();
         criteria.andEqualTo("categoryId",categoryId);
@@ -55,12 +73,99 @@ public class ContentServiceImpl extends BaseServiceImpl<TbContent>implements Con
         criteria.andEqualTo("status","1");
         // 降序排序
         example.orderBy("sortOrder").desc();
-        List<TbContent> list = contentMapper.selectByExample(example);
+         list = contentMapper.selectByExample(example);
 
+         // 设置某个分类对应的广告内容列表到缓存中
+
+        try {
+            redisTemplate.boundHashOps(REDIS_CONTENT).put(categoryId,list);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return list;
     }
 
 
+    /**
+    *新增广告清除缓存
+    * @Author: Timelin
+    * @Param : TbContent
+    * @Return : void
+    **/
+    @Override
+    public void add(TbContent content) {
+
+        super.add(content);
+        //更新内容分类对应在redis中的内容列表缓存
+        updateContentInRedisByCategoryId(content.getCategoryId());
+
+    }
+
+
+
+    /**
+    *修改广告后清除Redis缓存
+    * @Author: Timelin
+    * @Param : TbContent
+    * @Return : void
+    **/
+    @Override
+    public void update(TbContent tbContent) {
+        TbContent oldContent = super.findOne(tbContent.getId());
+
+        super.update(tbContent);
+
+        // 是否修改了内容分类，如果修改了内容分类则需要将新旧分类对应的内容列表都更新
+        if(!oldContent.getCategoryId().equals(tbContent.getCategoryId())){
+            updateContentInRedisByCategoryId(oldContent.getCategoryId());
+        }
+        updateContentInRedisByCategoryId(tbContent.getCategoryId());
+
+    }
+
+
+
+    /**
+    *批量删除广告后清除缓存
+    * @Author: Timelin
+    * @Param : Serializable[] ids
+    * @Return : void
+    **/
+    @Override
+    public void deleteByIds(Serializable[] ids) {
+
+        // 1 根据内容id集合查询内容列表，然后再更新该内容分类对应的内容列表缓存
+        Example example = new Example(TbContent.class);
+        example.createCriteria().andIn("id", Arrays.asList(ids));
+        List<TbContent> contentList = contentMapper.selectByExample(example);
+
+        if(contentList != null && contentList.size()>0){
+            for (TbContent content :contentList){
+                updateContentInRedisByCategoryId(content.getCategoryId());
+            }
+        }
+
+        // 2. 删除内容
+        super.deleteByIds(ids);
+
+
+    }
+
+    /**
+    *新旧分类对应的内容列表都更新
+    * @Author: Timelin
+    * @Param : Long categoryId
+    * @Return : void
+    **/
+    private void updateContentInRedisByCategoryId(Long categoryId) {
+
+        try {
+            redisTemplate.boundHashOps(REDIS_CONTENT).delete(categoryId);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 }
 
 
